0226db6f239ac778408faf7b03c261e8bea49e32
Ardyn Sulaeman <lemanfoetra@gmail.com>
2023-07-28 10:58:05 +0700

    First App With Vue

---
+++ app.js
@@ -0,0 +1,16 @@
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: '',
+        }
+    },
+    methods: {
+        addGoal() {
+            if (this.enteredValue != '') {
+                this.goals.push(this.enteredValue);
+            }
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
